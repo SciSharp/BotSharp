b5135ab2362a681a1b1f78a2712f1795feec1950
Oceania2018 <haiping008@gmail.com>
2018-08-13 11:12:10 -0500

    docker pull microsoft/aspnetcore-build

--- BotSharp.RestApi/Dialogflow/QueryController.cs
+++ BotSharp.RestApi/Dialogflow/QueryController.cs
@@ -2,20 +2,20 @@
 using BotSharp.Core.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BotSharp.RestApi.Dialogs
+namespace BotSharp.RestApi.Dialogflow
 {
+#if MODE_DIALOGFLOW
     /// <summary>
-    /// Conversation controller
+    /// Dialogflow mode query controller
     /// </summary>
     [Authorize]
     [Route("v1/[controller]")]
-    public class DialogController : ControllerBase
+    public class QueryController : ControllerBase
     {
         private readonly IBotPlatform _platform;
 
@@ -23,7 +23,7 @@ namespace BotSharp.RestApi.Dialogs
         /// Initialize dialog controller and get a platform instance
         /// </summary>
         /// <param name="platform"></param>
-        public DialogController(IBotPlatform platform)
+        public QueryController(IBotPlatform platform)
         {
             _platform = platform;
         }
@@ -31,11 +31,12 @@ namespace BotSharp.RestApi.Dialogs
         /// <summary>
         /// The query endpoint is used to process natural language in the form of text. 
         /// The query requests return structured data in JSON format with an action and parameters for that action.
+        /// Both GET and POST methods return the same JSON response.
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        [HttpPost("/v1/query")]
-        public ActionResult<AIResponse> Query([FromBody] QueryModel request)
+        [HttpGet, HttpPost]
+        public ActionResult<AIResponse> Query(QueryModel request)
         {
             String clientAccessToken = Request.Headers["ClientAccessToken"];
             var config = new AIConfiguration(clientAccessToken, SupportedLanguage.English);
@@ -50,8 +51,9 @@ namespace BotSharp.RestApi.Dialogs
                 Language = request.Lang,
                 Query = new String[] { request.Query }
             });
-            
+
             return aIResponse;
         }
     }
+#endif
 }
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
+# stage 1: build
+FROM microsoft/aspnetcore-build AS builder
+WORKDIR /source
+
+ # copies the rest of your code
+COPY . .
+RUN dotnet build
+RUN dotnet publish --output /app/ --configuration Release
+
+# stage 2: install
 FROM microsoft/aspnetcore
 WORKDIR /app
-COPY ./BotSharp.WebHost/PublishOutput .
-# ENTRYPOINT [ "dotnet", "BotSharp.WebHost.dll" ]
+COPY --from=builder /app .
+ENTRYPOINT [ "dotnet", "BotSharp.WebHost.dll" ]
