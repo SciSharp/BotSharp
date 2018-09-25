e20d7b48a3aa199502a51fc3732c1f59d80c02ab
Oceania2018 <haiping008@gmail.com>
2018-09-25 12:35:08 -0500

    Add entities and intents interface for Dialogflow mode.

---
+++ BotSharp.RestApi/Dialogflow/EntitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotSharp.RestApi.Dialogflow
+{
+#if DIALOGFLOW
+    /// <summary>
+    /// The /entities endpoint is used to create, retrieve, update, and delete developer-defined entity objects.
+    /// </summary>
+    [Authorize]
+    [Route("v1/[controller]")]
+    public class EntitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<DialogflowAllEntityViewModel> All()
+        {
+            return new List<DialogflowAllEntityViewModel>();
+        }
+    }
+#endif
+}
