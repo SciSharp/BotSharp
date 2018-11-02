4958da5c21b5a3e98fafc69cbd9ccafbe8b9e466
Oceania2018 <haiping008@gmail.com>
2018-11-02 07:53:27 -0500

    Build-in Facebook Messenger channel.

---
+++ BotSharp.Channel.FacebookMessenger/Models/WebhookMessageQuickReply.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotSharp.Channel.FacebookMessenger.Models
+{
+    public class WebhookMessageQuickReply
+    {
+        public String Payload { get; set; }
+    }
+}
