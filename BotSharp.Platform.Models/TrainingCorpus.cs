4c28fb0ca5ed274f2272945f16508ba082472bd0
geffzhang <zsygz@hotmail.com>
2018-11-06 13:58:38 +0800

    fix TrainingCorpus

--- BotSharp.Platform.Models/TrainingCorpus.cs
+++ BotSharp.Platform.Models/TrainingCorpus.cs
@@ -6,6 +6,12 @@ namespace BotSharp.Platform.Models
 {
     public class TrainingCorpus
     {
+        public TrainingCorpus()
+        {
+            UserSays = new List<TrainingIntentExpression<TrainingIntentExpressionPart>>();
+            Entities = new List<TrainingEntity>();
+        }
+
         public List<TrainingIntentExpression<TrainingIntentExpressionPart>> UserSays { get; set; }
 
         /// <summary>
