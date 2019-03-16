870bd7a5a88560e6a323f33f6ae078e5507bbd01
Oceania2018 <haiping008@gmail.com>
2019-03-16 11:26:04 -0500

    remove BotSharp.UI project.

--- BotSharp.UI/src/config/config.js
+++
@@ -1,8 +0,0 @@
-import Env from './env';
-
-let config = {
-	env: Env,
-	baseURL: (Env == 'development' ? `http://localhost:3112` : `http://localhost:3112`),
-	testAccount: {username: `support@botsharp.io`, password: (Env == 'development' ? `botsharp` : ``)}
-};
-export default config;
