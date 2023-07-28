861cbb7fe2d86d19707752dd5e84a4f1d060f151
hchen2020 <101423@smsassist.com>
2023-07-27 22:12:12 -0500

    Print Azure AI settings.

--- src/Infrastructure/BotSharp.Abstraction/Utilities/StringExtensions.cs
+++ src/Infrastructure/BotSharp.Abstraction/Utilities/StringExtensions.cs
@@ -4,4 +4,15 @@ public static class StringExtensions
 {
     public static string IfNullOrEmptyAs(this string str, string defaultValue)
         => string.IsNullOrEmpty(str) ? defaultValue : str;
+
+    public static string SubstringMax(this string str, int maxLength)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        if (str.Length > maxLength)
+            return str.Substring(0, maxLength);
+        else
+            return str;
+    }
 }
