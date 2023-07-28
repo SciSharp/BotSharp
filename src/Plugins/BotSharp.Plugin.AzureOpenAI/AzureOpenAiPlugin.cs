de2587dcb5fc4fa5f48c9188960e31976007cf44
hchen2020 <101423@smsassist.com>
2023-07-27 22:12:12 -0500

    Print Azure AI settings.

--- src/Plugins/BotSharp.Plugin.AzureOpenAI/AzureOpenAiPlugin.cs
+++ src/Plugins/BotSharp.Plugin.AzureOpenAI/AzureOpenAiPlugin.cs
@@ -1,9 +1,11 @@
 using BotSharp.Abstraction.MLTasks;
 using BotSharp.Abstraction.Plugins;
+using BotSharp.Abstraction.Utilities;
 using BotSharp.Plugin.AzureOpenAI.Providers;
 using BotSharp.Plugin.AzureOpenAI.Settings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace BotSharp.Platform.AzureAi;
 
@@ -13,7 +15,11 @@ public class AzureOpenAiPlugin : IBotSharpPlugin
     {
         var settings = new AzureOpenAiSettings();
         config.Bind("AzureOpenAi", settings);
-        services.AddSingleton(x => settings);
+        services.AddSingleton(x =>
+        {
+            Console.WriteLine($"Loaded AzureOpenAi settings: {settings.DeploymentModel} ({settings.Endpoint}) {settings.ApiKey.SubstringMax(4)}");
+            return settings;
+        });
 
         services.AddScoped<ITextCompletion, TextCompletionProvider>();
         services.AddScoped<IChatCompletion, ChatCompletionProvider>();
--- src/Plugins/BotSharp.Plugin.AzureOpenAI/Settings/DeploymentModelSetting.cs
+++ src/Plugins/BotSharp.Plugin.AzureOpenAI/Settings/DeploymentModelSetting.cs
@@ -4,4 +4,9 @@ public class DeploymentModelSetting
 {
     public string? ChatCompletionModel { get; set; }
     public string? TextCompletionModel { get; set; }
+
+    public override string ToString()
+    {
+        return $"ChatCompletion - {ChatCompletionModel}, TextCompletion - {TextCompletionModel}";
+    }
 }
