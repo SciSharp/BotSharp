78b33bd4de746e770dcd0ec6b5cb61e458d845cf
hchen2020 <101423@smsassist.com>
2023-08-21 23:49:42 -0500

    Apply centralized routing by routing table.

--- src/Infrastructure/BotSharp.Abstraction/Agents/Models/AgentRoutingArgs.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace BotSharp.Abstraction.Agents.Models;
-
-public class AgentRoutingArgs
-{
-    [JsonPropertyName("agent_id")]
-    public string AgentId { get; set; }
-}
---
+++ src/Infrastructure/BotSharp.Abstraction/Agents/Models/RoutingResult.cs
@@ -0,0 +1,11 @@
+namespace BotSharp.Abstraction.Agents.Models;
+
+public class RoutingResult
+{
+    public string Result { get; set; }
+
+    public RoutingResult(string result)
+    {
+        Result = result;
+    }
+}
