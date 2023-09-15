c54e0353d830cebfbd333391ddd330ee86d0c1f8
hchen2020 <101423@smsassist.com>
2023-09-15 14:42:14 -0500

    Get similar work orders before wo creation.

--- src/Plugins/BotSharp.Plugin.KnowledgeBase/MemVecDb/VecRecord.cs
+++ src/Plugins/BotSharp.Plugin.KnowledgeBase/MemVecDb/VecRecord.cs
@@ -5,4 +5,9 @@ public class VecRecord
     public int Id { get; set; }
     public float[] Vector { get; set; }
     public string Text { get; set; }
+
+    public override string ToString()
+    {
+        return Text;
+    }
 }
