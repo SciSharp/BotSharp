74259ee6bc3144042cb58ddd261feba8bd93bd18
hchen <hchen@smsassist.com>
2023-11-09 14:02:57 -0600

    Add Twilio plugin.

---
+++ src/Plugins/BotSharp.Plugin.Twilio/Settings/TwilioSetting.cs
@@ -0,0 +1,9 @@
+namespace BotSharp.Plugin.Twilio.Settings;
+
+public class TwilioSetting
+{
+    public string PhoneNumber { get; set; }
+    public string AccountSID { get; set; }
+    public string AuthToken { get; set; }
+    public string CallbackHost { get; set; }
+}
---
+++ src/Plugins/BotSharp.Plugin.Twilio/Using.cs
@@ -0,0 +1,17 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Linq;
+global using System.Text.Json;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Logging;
+global using BotSharp.Abstraction.Functions;
+global using BotSharp.Abstraction.Functions.Models;
+global using BotSharp.Abstraction.Agents.Enums;
+global using BotSharp.Abstraction.Models;
+global using BotSharp.Abstraction.Agents;
+global using BotSharp.Abstraction.Conversations;
+global using BotSharp.Abstraction.Plugins;
+global using BotSharp.Abstraction.Conversations.Models;
