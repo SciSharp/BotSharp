7824622385cbe2d3a157061ac7debe36df4a76a7
Haiping Chen <haiping008@gmail.com>
2023-11-13 19:25:46 -0600

    Add role to User.

---
+++ src/Infrastructure/BotSharp.Abstraction/Users/Enums/UserRole.cs
@@ -0,0 +1,19 @@
+namespace BotSharp.Abstraction.Users.Enums;
+
+public class UserRole
+{
+    /// <summary>
+    /// Admin account
+    /// </summary>
+    public const string Admin = "admin";
+
+    /// <summary>
+    /// Customer service representative (CSR)
+    /// </summary>
+    public const string CSR = "csr";
+
+    /// <summary>
+    /// Client
+    /// </summary>
+    public const string Client = "client";
+}
