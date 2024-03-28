ffe9fb81b001747b0d99d4627d39b9a0abf6f7ce
C. Oceania <haiping008@gmail.com>
2024-03-28 16:44:57 -0500

    Update RoutingService.InvokeFunction.cs

--- src/Infrastructure/BotSharp.Core/Routing/RoutingService.InvokeFunction.cs
+++ src/Infrastructure/BotSharp.Core/Routing/RoutingService.InvokeFunction.cs
@@ -69,11 +69,11 @@ public partial class RoutingService
         }
 
         // Save to Storage as well
-        if (!message.StopCompletion && message.FunctionName != "route_to_agent")
+        /*if (!message.StopCompletion && message.FunctionName != "route_to_agent")
         {
             var storage = _services.GetRequiredService<IConversationStorage>();
             storage.Append(Context.ConversationId, message);
-        }
+        }*/
 
         return result;
     }
