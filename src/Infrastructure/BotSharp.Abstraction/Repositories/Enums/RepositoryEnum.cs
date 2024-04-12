e319992206bcf801b63b160f8c34e26954556db5
Jicheng Lu <103353@smsassist.com>
2024-04-12 10:00:55 -0500

    add repository enum

---
+++ src/Infrastructure/BotSharp.Abstraction/Repositories/Enums/RepositoryEnum.cs
@@ -0,0 +1,7 @@
+namespace BotSharp.Abstraction.Repositories.Enums;
+
+public static class RepositoryEnum
+{
+    public const string FileRepository = nameof(FileRepository);
+    public const string MongoRepository = nameof(MongoRepository);
+}
