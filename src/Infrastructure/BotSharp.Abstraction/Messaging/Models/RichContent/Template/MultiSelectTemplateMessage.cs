59958bcf8bb45f12ada02301f348fb6cb7134df8
Jicheng Lu <103353@smsassist.com>
2024-04-18 13:54:43 -0500

    refine json

--- src/Infrastructure/BotSharp.Abstraction/Messaging/Models/RichContent/Template/MultiSelectTemplateMessage.cs
+++ src/Infrastructure/BotSharp.Abstraction/Messaging/Models/RichContent/Template/MultiSelectTemplateMessage.cs
@@ -1,21 +1,28 @@
 using BotSharp.Abstraction.Messaging.Enums;
+using Newtonsoft.Json;
 
 namespace BotSharp.Abstraction.Messaging.Models.RichContent.Template;
 
 public class MultiSelectTemplateMessage : IRichMessage, ITemplateMessage
 {
     [JsonPropertyName("rich_type")]
+    [JsonProperty("rich_type")]
     public string RichType => RichTypeEnum.MultiSelectTemplate;
+
     [JsonPropertyName("text")]
+    [JsonProperty("text")]
     public string Text { get; set; } = string.Empty;
 
     [JsonPropertyName("template_type")]
+    [JsonProperty("template_type")]
     public string TemplateType => TemplateTypeEnum.MultiSelect;
 
     [JsonPropertyName("options")]
+    [JsonProperty("options")]
     public List<OptionElement> Options { get; set; } = new List<OptionElement>();
 
     [JsonPropertyName("is_horizontal")]
+    [JsonProperty("is_horizontal")]
     public bool IsHorizontal { get; set; }
 }
 
--- src/Infrastructure/BotSharp.Abstraction/Messaging/Models/RichContent/TextMessage.cs
+++ src/Infrastructure/BotSharp.Abstraction/Messaging/Models/RichContent/TextMessage.cs
@@ -1,10 +1,12 @@
 using BotSharp.Abstraction.Messaging.Enums;
+using Newtonsoft.Json;
 
 namespace BotSharp.Abstraction.Messaging.Models.RichContent;
 
 public class TextMessage : IRichMessage
 {
     [JsonPropertyName("rich_type")]
+    [JsonProperty("rich_type")]
     public string RichType => RichTypeEnum.Text;
 
     public string Text { get; set; } = string.Empty;
