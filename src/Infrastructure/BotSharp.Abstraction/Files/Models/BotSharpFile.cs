7e908d33868a8a972ed18663929312bf747584c9
Jicheng Lu <103353@smsassist.com>
2024-05-14 11:55:11 -0500

    add comment

--- src/Infrastructure/BotSharp.Abstraction/Files/Models/BotSharpFile.cs
+++ src/Infrastructure/BotSharp.Abstraction/Files/Models/BotSharpFile.cs
@@ -6,6 +6,9 @@ public class BotSharpFile
     [JsonPropertyName("file_name")]
     public string FileName { get; set; } = string.Empty;
 
+    /// <summary>
+    /// File data, e.g., "data:image/png;base64,aaaaaaaa"
+    /// </summary>
     [JsonPropertyName("file_data")]
     public string FileData { get; set; } = string.Empty;
 
