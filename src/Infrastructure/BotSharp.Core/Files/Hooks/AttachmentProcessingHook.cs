ff5a553296d34c07d50bf06279aa5ed0abea330f
Jicheng Lu <franklujc@gmail.com>
2024-06-19 04:24:49 -0500

    refine load attachment

--- src/Infrastructure/BotSharp.Core/Files/Hooks/AttachmentProcessingHook.cs
+++ src/Infrastructure/BotSharp.Core/Files/Hooks/AttachmentProcessingHook.cs
@@ -1,10 +1,11 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace BotSharp.Core.Files.Hooks;
 
 public class AttachmentProcessingHook : AgentHookBase
 {
     private readonly IServiceProvider _services;
-    private readonly AgentSettings _agentSettings;
 
     public override string SelfId => string.Empty;
 
@@ -12,7 +13,6 @@ public class AttachmentProcessingHook : AgentHookBase
         : base(services, settings)
     {
         _services = services;
-        _agentSettings = settings;
     }
 
     public override void OnAgentLoaded(Agent agent)
@@ -23,39 +23,29 @@ public class AttachmentProcessingHook : AgentHookBase
 
         if (hasConvFiles)
         {
-            agent.Instruction += "\r\n\r\nIf user wants to describe images or pdf files, please call load_attachment.";
-        }
+            agent.Instruction += "\r\n\r\nPlease call load_attachment if user wants to describe files, such as images, pdf.\r\n\r\n";
 
-        base.OnAgentLoaded(agent);
-    }
-
-    public override bool OnFunctionsLoaded(List<FunctionDef> functions)
-    {
-        var fileService = _services.GetRequiredService<IBotSharpFileService>();
-        var conv = _services.GetRequiredService<IConversationService>();
-        var hasConvFiles = fileService.HasConversationUserFiles(conv.ConversationId);
-
-        if (hasConvFiles)
-        {
-            var json = JsonSerializer.Serialize(new
+            if (agent.Functions != null)
             {
-                user_request = new
+                var json = JsonSerializer.Serialize(new
                 {
-                    type = "string",
-                    description = "The request posted by user, which is related to analyzing requested files. User can request for multiple files to process at one time."
-                },
-                file_types = new
-                {
-                    type = "string",
-                    description = "The file types requested by user to analyze, such as image, png, jpeg, and pdf. There can be multiple file types in a single request. An example output is, 'image,pdf'"
-                }
-            });
+                    user_request = new
+                    {
+                        type = "string",
+                        description = "The request posted by user, which is related to analyzing requested files. User can request for multiple files to process at one time."
+                    },
+                    file_types = new
+                    {
+                        type = "string",
+                        description = "The file types requested by user to analyze, such as image, png, jpeg, and pdf. There can be multiple file types in a single request. An example output is, 'image,pdf'"
+                    }
+                });
 
-            functions.Add(new FunctionDef
-            {
-                Name = "load_attachment",
-                Description = "If the user's request is related to analyzing files and/or images, you can call this function to analyze files and images.",
-                Parameters =
+                agent.Functions.Add(new FunctionDef
+                {
+                    Name = "load_attachment",
+                    Description = "If the user's request is related to analyzing files and/or images, you can call this function to analyze files and images.",
+                    Parameters =
                     {
                         Properties = JsonSerializer.Deserialize<JsonDocument>(json),
                         Required = new List<string>
@@ -64,8 +54,10 @@ public class AttachmentProcessingHook : AgentHookBase
                             "file_types"
                         }
                     }
-            });
+                });
+            }
         }
-        return base.OnFunctionsLoaded(functions); ;
+
+        base.OnAgentLoaded(agent);
     }
 }
