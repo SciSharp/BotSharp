adfdefa98a1f175ad32261d39e40294b1865336d
Jicheng Lu <103353@smsassist.com>
2024-07-17 13:31:09 -0500

    extend email with attachments

--- src/Plugins/BotSharp.Plugin.EmailHandler/Functions/HandleEmailRequestFn.cs
+++ src/Plugins/BotSharp.Plugin.EmailHandler/Functions/HandleEmailRequestFn.cs
@@ -1,12 +1,7 @@
-using BotSharp.Abstraction.Email.Settings;
-using BotSharp.Plugin.EmailHandler.LlmContexts;
-using MailKit;
 using MailKit.Net.Smtp;
 using MailKit.Security;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using MimeKit;
-using System.Net.Http;
+using System.IO;
 
 namespace BotSharp.Plugin.EmailHandler.Functions;
 
@@ -22,12 +17,13 @@ public class HandleEmailRequestFn : IFunctionCallback
     private readonly BotSharpOptions _options;
     private readonly EmailHandlerSettings _emailSettings;
 
-    public HandleEmailRequestFn(IServiceProvider services,
-                ILogger<HandleEmailRequestFn> logger,
-                IHttpClientFactory httpClientFactory,
-                IHttpContextAccessor context,
-                BotSharpOptions options,
-                EmailHandlerSettings emailPluginSettings)
+    public HandleEmailRequestFn(
+        IServiceProvider services,
+        ILogger<HandleEmailRequestFn> logger,
+        IHttpClientFactory httpClientFactory,
+        IHttpContextAccessor context,
+        BotSharpOptions options,
+        EmailHandlerSettings emailPluginSettings)
     {
         _services = services;
         _logger = logger;
@@ -42,6 +38,8 @@ public class HandleEmailRequestFn : IFunctionCallback
         var recipient = args?.ToAddress;
         var body = args?.Content;
         var subject = args?.Subject;
+        var isNeedAttachments = args?.IsNeedAttachemnts ?? false;
+        var bodyBuilder = new BodyBuilder();
 
         try
         {
@@ -49,13 +47,19 @@ public class HandleEmailRequestFn : IFunctionCallback
             mailMessage.From.Add(new MailboxAddress(_emailSettings.Name, _emailSettings.EmailAddress));
             mailMessage.To.Add(new MailboxAddress("", recipient));
             mailMessage.Subject = subject;
-            mailMessage.Body = new TextPart("plain")
+            bodyBuilder.TextBody = body;
+
+            if (isNeedAttachments)
             {
-                Text = body
-            };
+                var files = await GetConversationFiles();
+                BuildEmailAttachments(bodyBuilder, files);
+            }
+
+            mailMessage.Body = bodyBuilder.ToMessageBody();
             var response = await HandleSendEmailBySMTP(mailMessage);
-            _logger.LogWarning($"Email successfully send over to {recipient}. Email Subject: {subject} [{response}]");
             message.Content = response;
+
+            _logger.LogWarning($"Email successfully send over to {recipient}. Email Subject: {subject} [{response}]");
             return true;
         }
         catch (Exception ex)
@@ -67,7 +71,76 @@ public class HandleEmailRequestFn : IFunctionCallback
         }
     }
 
-    public async Task<string> HandleSendEmailBySMTP(MimeMessage mailMessage)
+    private async Task<IEnumerable<MessageFileModel>> GetConversationFiles()
+    {
+        var convService = _services.GetService<IConversationService>();
+        var fileService = _services.GetRequiredService<IBotSharpFileService>();
+        var conversationId = convService.ConversationId;
+        var dialogs = convService.GetDialogHistory(fromBreakpoint: false);
+        var messageIds = dialogs.Select(x => x.MessageId).Distinct().ToList();
+        var files = fileService.GetMessageFiles(conversationId, messageIds, FileSourceType.User);
+        return await SelectFiles(files, dialogs);
+    }
+
+    private async Task<IEnumerable<MessageFileModel>> SelectFiles(IEnumerable<MessageFileModel> files, List<RoleDialogModel> dialogs)
+    {
+        if (files.IsNullOrEmpty()) return new List<MessageFileModel>();
+
+        var llmProviderService = _services.GetRequiredService<ILlmProviderService>();
+        var render = _services.GetRequiredService<ITemplateRender>();
+        var db = _services.GetRequiredService<IBotSharpRepository>();
+
+        try
+        {
+            var promptFiles = files.Select((x, idx) =>
+            {
+                return $"id: {idx + 1}, file_name: {x.FileName}.{x.FileType}, content_type: {x.ContentType}";
+            }).ToList();
+            var prompt = db.GetAgentTemplate(BuiltInAgentId.UtilityAssistant, "email_attachment_prompt");
+            prompt = render.Render(prompt, new Dictionary<string, object>
+            {
+                { "file_list", promptFiles }
+            });
+
+            var agent = new Agent
+            {
+                Id = BuiltInAgentId.UtilityAssistant,
+                Name = "Utility Assistant",
+                Instruction = prompt
+            };
+
+            var provider = llmProviderService.GetProviders().FirstOrDefault(x => x == "openai");
+            var model = llmProviderService.GetProviderModel(provider: provider, id: "gpt-4", multiModal: true);
+            var completion = CompletionProvider.GetChatCompletion(_services, provider: provider, model: model.Name);
+            var response = await completion.GetChatCompletions(agent, dialogs);
+            var content = response?.Content ?? string.Empty;
+            var fids = JsonSerializer.Deserialize<List<int>>(content) ?? new List<int>();
+            return files.Where((x, idx) => fids.Contains(idx + 1)).ToList();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning($"Error when getting the email file response. {ex.Message}\r\n{ex.InnerException}");
+            return new List<MessageFileModel>();
+        }
+    }
+
+    private void BuildEmailAttachments(BodyBuilder builder, IEnumerable<MessageFileModel> files)
+    {
+        if (files.IsNullOrEmpty()) return;
+
+        foreach (var file in files)
+        {
+            if (string.IsNullOrEmpty(file.FileStorageUrl)) continue;
+
+            using var fs = File.OpenRead(file.FileStorageUrl);
+            var binary = BinaryData.FromStream(fs);
+            builder.Attachments.Add($"{file.FileName}.{file.FileType}", binary.ToArray(), ContentType.Parse(file.ContentType));
+            fs.Close();
+            Thread.Sleep(100);
+        }
+    }
+
+    private async Task<string> HandleSendEmailBySMTP(MimeMessage mailMessage)
     {
         using var smtpClient = new SmtpClient();
         await smtpClient.ConnectAsync(_emailSettings.SMTPServer, _emailSettings.SMTPPort, SecureSocketOptions.StartTls);
