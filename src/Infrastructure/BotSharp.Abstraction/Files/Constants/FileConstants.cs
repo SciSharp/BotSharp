2ecd6c3649393671ad7c7c6c21a4ed1e837eceda
Jicheng Lu <103353@smsassist.com>
2024-08-02 16:24:44 -0500

    fix audio range request

---
+++ src/Infrastructure/BotSharp.Abstraction/Files/Constants/FileConstants.cs
@@ -0,0 +1,9 @@
+namespace BotSharp.Abstraction.Files.Constants;
+
+public class FileConstants
+{
+    public static readonly IEnumerable<string> AudioTypes = new List<string>
+    {
+        ".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma"
+    };
+}
