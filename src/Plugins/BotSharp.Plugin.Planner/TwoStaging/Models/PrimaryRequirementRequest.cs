348888ae638957edc9fe46fad9a96e0c02bc6a2e
Joanna Ren <101223@smsassist.com>
2024-09-04 15:58:03 -0500

    Improve Primary Planning

--- src/Plugins/BotSharp.Plugin.Planner/TwoStaging/Models/PrimaryRequirementRequest.cs
+++ src/Plugins/BotSharp.Plugin.Planner/TwoStaging/Models/PrimaryRequirementRequest.cs
@@ -5,9 +5,6 @@ public class PrimaryRequirementRequest
     [JsonPropertyName("requirement_detail")]
     public string Requirements { get; set; } = null!;
 
-    [JsonPropertyName("has_knowledge_reference")]
-    public bool HasKnowledgeReference { get; set; }
-
-    [JsonPropertyName("question")]
-    public string Question { get; set; } = null!;
+    [JsonPropertyName("questions")]
+    public string[] Questions { get; set; } = [];
 }
