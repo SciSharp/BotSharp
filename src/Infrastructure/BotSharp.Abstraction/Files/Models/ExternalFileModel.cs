c2a5b6eeeabcc47d6a9bb8ae2f6998ae45a90b66
Jicheng Lu <103353@smsassist.com>
2024-09-12 11:53:10 -0500

    minor change

--- src/Infrastructure/BotSharp.Abstraction/Files/Models/ExternalFileModel.cs
+++ src/Infrastructure/BotSharp.Abstraction/Files/Models/ExternalFileModel.cs
@@ -4,4 +4,10 @@ public class ExternalFileModel : FileDataModel
 {
     [JsonPropertyName("file_url")]
     public string? FileUrl { get; set; }
+
+    /// <summary>
+    /// File data => format: "data:image/png;base64,aaaaaaaa"
+    /// </summary>
+    [JsonPropertyName("file_data")]
+    public new string? FileData { get; set; }
 }
