b82e1bda2d8942f1b5896aaa7b03eae100017e96
Wenbo Cao <104199@smsassist.com>
2024-09-26 15:34:16 -0500

    sqlite version for excel handler

---
+++ src/Plugins/BotSharp.Plugin.ExcelHandler/ExcelHandlerPlugin.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BotSharp.Abstraction.Plugins;
+using BotSharp.Abstraction.Settings;
+using BotSharp.Plugin.ExcelHandler.Helpers;
+using BotSharp.Plugin.ExcelHandler.Hooks;
+using BotSharp.Plugin.ExcelHandler.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace BotSharp.Plugin.ExcelHandler;
+
+public class ExcelHandlerPlugin : IBotSharpPlugin
+{
+    public string Id => "c56a8e29-b16f-4d75-8766-8309342130cb";
+    public string Name => "Excel Handler";
+    public string Description => "Load data from excel file and transform it into a list of JSON format.";
+
+    public void RegisterDI(IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped(provider =>
+        {
+            var settingService = provider.GetRequiredService<ISettingService>();
+            return settingService.Bind<ExcelHandlerSettings>("ExcelHandler");
+        });
+
+        services.AddScoped<IAgentUtilityHook, ExcelHandlerUtilityHook>();
+        services.AddScoped<IAgentHook, ExcelHandlerHook>();
+        services.AddScoped<IDbHelpers, DbHelpers>();
+    }
+}
