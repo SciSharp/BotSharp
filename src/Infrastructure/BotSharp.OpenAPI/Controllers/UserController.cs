2c77ad0798d4ef30e4a27e74d2001862af7eba8b
AnonymousDotNet <18776095145@163.com>
2024-09-29 13:14:52 +0800

    remove Unauthorized

--- src/Infrastructure/BotSharp.OpenAPI/Controllers/UserController.cs
+++ src/Infrastructure/BotSharp.OpenAPI/Controllers/UserController.cs
@@ -68,7 +68,6 @@ public class UserController : ControllerBase
         var token = await _userService.ActiveUser(model);
         if (token == null)
         {
-            //return Unauthorized();
             return BadRequest();
         }
         return Ok(token);
