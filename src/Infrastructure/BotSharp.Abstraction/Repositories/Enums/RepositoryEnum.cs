6bec201ddf0414d53dd0f4d56efd117df97a229d
Gil Zhang <gil.zhang.dev@outlook.com>
2024-10-09 00:22:01 +0800

    update BotSharpDatabaseSettings

--- src/Infrastructure/BotSharp.Abstraction/Repositories/Enums/RepositoryEnum.cs
+++ src/Infrastructure/BotSharp.Abstraction/Repositories/Enums/RepositoryEnum.cs
@@ -4,4 +4,6 @@ public static class RepositoryEnum
 {
     public const string FileRepository = nameof(FileRepository);
     public const string MongoRepository = nameof(MongoRepository);
+    public const string PostgreSqlRepository = nameof(PostgreSqlRepository);
+    public const string MySqlRepository = nameof(MySqlRepository);
 }
