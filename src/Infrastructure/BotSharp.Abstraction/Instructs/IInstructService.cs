e62a38eb5be2d880916b3731894c01520460c707
Jicheng Lu <103353@smsassist.com>
2024-10-17 18:17:38 -0500

    add instruct

--- src/Infrastructure/BotSharp.Abstraction/Instructs/IInstructService.cs
+++ src/Infrastructure/BotSharp.Abstraction/Instructs/IInstructService.cs
@@ -4,5 +4,16 @@ namespace BotSharp.Abstraction.Instructs;
 
 public interface IInstructService
 {
+    /// <summary>
+    /// Execute completion by using specified instruction or template
+    /// </summary>
+    /// <param name="agentId">Agent (static agent)</param>
+    /// <param name="message">Additional message provided by user</param>
+    /// <param name="templateName">Template name</param>
+    /// <param name="instruction">System prompt</param>
+    /// <returns></returns>
     Task<InstructResult> Execute(string agentId, RoleDialogModel message, string? templateName = null, string? instruction = null);
+
+    
+    Task<T?> Instruct<T>(string instruction, string agentId, InstructOptions options) where T : class;
 }
