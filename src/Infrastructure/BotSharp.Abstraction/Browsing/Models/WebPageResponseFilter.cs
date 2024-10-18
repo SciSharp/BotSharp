d481928f168916d6e2852fa5774302fe8ede4b75
Haiping Chen <haiping008@gmail.com>
2024-10-18 11:32:41 -0500

    Product V3

--- src/Infrastructure/BotSharp.Abstraction/Browsing/Models/WebPageResponseFilter.cs
+++ src/Infrastructure/BotSharp.Abstraction/Browsing/Models/WebPageResponseFilter.cs
@@ -4,4 +4,14 @@ public class WebPageResponseFilter
 {
     public string Url { get; set; } = null!;
     public string[]? QueryParameters { get; set; }
+
+    /// <summary>
+    /// contains, starts, ends, equals
+    /// </summary>
+    public string UrlMatchPattern { get; set; } = "contains";
+
+    /// <summary>
+    /// Handle Content-Type: text/x-component
+    /// </summary>
+    public int PartIndex { get; set; } = -1;
 }
