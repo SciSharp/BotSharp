e215dec55afe7d77feae66bc4f3b434816b1ad43
Jicheng Lu <103353@smsassist.com>
2024-11-07 17:42:13 -0600

    init conv simulation

--- src/Infrastructure/BotSharp.Abstraction/Evaluations/Models/EvaluationRequest.cs
+++ src/Infrastructure/BotSharp.Abstraction/Evaluations/Models/EvaluationRequest.cs
@@ -1,6 +1,18 @@
+using BotSharp.Abstraction.Processors.Models;
+
 namespace BotSharp.Abstraction.Evaluations.Models;
 
-public class EvaluationRequest
+public class EvaluationRequest : LlmBaseRequest
 {
-    public string AgentId { get; set; }
+    [JsonPropertyName("agent_id")]
+    public new string AgentId { get; set; }
+
+    [JsonPropertyName("states")]
+    public IEnumerable<MessageState> States { get; set; } = [];
+
+    [JsonPropertyName("max_rounds")]
+    public int MaxRounds { get; set; } = 20;
+
+    [JsonPropertyName("ref_conversation_id")]
+    public string RefConversationId { get; set; } = null!;
 }
