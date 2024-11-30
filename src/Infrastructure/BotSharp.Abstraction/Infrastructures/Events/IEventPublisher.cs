5f31ffe6587e3321238ee346146b938feff5dff2
Haiping Chen <haiping008@gmail.com>
2024-11-30 15:16:04 +0000

    Allow Event to be removed.

--- src/Infrastructure/BotSharp.Abstraction/Infrastructures/Events/IEventPublisher.cs
+++ src/Infrastructure/BotSharp.Abstraction/Infrastructures/Events/IEventPublisher.cs
@@ -15,4 +15,6 @@ public interface IEventPublisher
     Task ReDispatchAsync(string channel, int count = 10, string order = "asc");
 
     Task ReDispatchPendingAsync(string channel, string group, int count = 10);
+
+    Task RemoveAsync(string channel, int count = 10);
 }
