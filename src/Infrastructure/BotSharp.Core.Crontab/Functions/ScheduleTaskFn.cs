5c7af586ecea191f4c81b2c8b1499277354ff6e6
Haiping Chen <hchen@lessen.com>
2024-12-11 16:02:22 -0600

    crontab only allow 1 minute.

--- src/Infrastructure/BotSharp.Core.Crontab/Functions/ScheduleTaskFn.cs
+++ src/Infrastructure/BotSharp.Core.Crontab/Functions/ScheduleTaskFn.cs
@@ -18,22 +18,37 @@ public class ScheduleTaskFn : IFunctionCallback
     public async Task<bool> Execute(RoleDialogModel message)
     {
         var args = JsonSerializer.Deserialize<ScheduleTaskArgs>(message.FunctionArgs);
+        if (args.LessThan60Seconds)
+        {
+            message.Content = "Cron expression should not include seconds.";
+            return false;
+        }
 
         var routing = _services.GetRequiredService<IRoutingContext>();
         var user = _services.GetRequiredService<IUserIdentity>();
-        var crontabItem = new CrontabItem
-        {
-            Title = args.Title,
-            Description = args.Description,
-            Cron = args.Cron,
-            UserId = user.Id,
-            AgentId = routing.EntryAgentId,
-            ConversationId = routing.ConversationId,
-            Tasks = args.Tasks,
-        };
-
         var db = _services.GetRequiredService<IBotSharpRepository>();
-        // var ret = db.UpsertCrontabItem(crontabItem);
+
+        if (string.IsNullOrEmpty(args.Cron))
+        {
+            var ret = db.DeleteCrontabItem(routing.ConversationId);
+            message.Content = $"Task schedule canceled result: {ret}";
+        }
+        else
+        {
+            var crontabItem = new CrontabItem
+            {
+                Title = args.Title,
+                Description = args.Description,
+                Cron = args.Cron,
+                UserId = user.Id,
+                AgentId = routing.EntryAgentId,
+                ConversationId = routing.ConversationId,
+                Tasks = args.Tasks,
+            };
+
+            var ret = db.UpsertCrontabItem(crontabItem);
+            message.Content = $"Task scheduled result: {ret}";
+        }
 
         return true;
     }
