a09eaffe9217b438e36c54c6e6212cfea7ee5457
Jicheng Lu <103353@smsassist.com>
2025-01-08 11:07:13 -0600

    rename

--- src/Infrastructure/BotSharp.Abstraction/Agents/Models/AgentRule.cs
+++ src/Infrastructure/BotSharp.Abstraction/Agents/Models/AgentRule.cs
@@ -2,7 +2,10 @@ namespace BotSharp.Abstraction.Agents.Models;
 
 public class AgentRule
 {
-    public string Name { get; set; }
+    [JsonPropertyName("trigger_name")]
+    public string TriggerName { get; set; }
+
+    [JsonPropertyName("disabled")]
     public bool Disabled { get; set; }
 
     [JsonPropertyName("event_name")]
