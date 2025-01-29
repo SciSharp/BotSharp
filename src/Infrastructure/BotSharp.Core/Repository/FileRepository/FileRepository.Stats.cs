ae8796e8c5ed7b62fea997d2a6ccf36d6e614dbd
Jicheng Lu <franklujc@gmail.com>
2025-01-28 23:37:06 -0600

    relocate

--- src/Infrastructure/BotSharp.Core/Repository/FileRepository/FileRepository.Stats.cs
+++ src/Infrastructure/BotSharp.Core/Repository/FileRepository/FileRepository.Stats.cs
@@ -7,7 +7,7 @@ public partial class FileRepository
     public BotSharpStats? GetGlobalStats(string metric, string dimension, DateTime recordTime, StatsInterval interval)
     {
         var baseDir = Path.Combine(_dbSettings.FileRepository, STATS_FOLDER);
-        var (startTime, endTime) = BuildTimeInterval(recordTime, interval);
+        var (startTime, endTime) = BotSharpStats.BuildTimeInterval(recordTime, interval);
         var dir = Path.Combine(baseDir, metric, startTime.Year.ToString(), startTime.Month.ToString("D2"));
         if (!Directory.Exists(dir)) return null;
 
@@ -27,7 +27,7 @@ public partial class FileRepository
     public bool SaveGlobalStats(BotSharpStats body)
     {
         var baseDir = Path.Combine(_dbSettings.FileRepository, STATS_FOLDER);
-        var (startTime, endTime) = BuildTimeInterval(body.RecordTime, body.IntervalType);
+        var (startTime, endTime) = BotSharpStats.BuildTimeInterval(body.RecordTime, body.IntervalType);
         body.StartTime = startTime;
         body.EndTime = endTime;
 
@@ -76,38 +76,4 @@ public partial class FileRepository
 
         return true;
     }
-
-    #region Private methods
-    private (DateTime, DateTime) BuildTimeInterval(DateTime recordTime, StatsInterval interval)
-    {
-        DateTime startTime = recordTime;
-        DateTime endTime = DateTime.UtcNow;
-
-        switch (interval)
-        {
-            case StatsInterval.Hour:
-                startTime = new DateTime(recordTime.Year, recordTime.Month, recordTime.Day, recordTime.Hour, 0, 0);
-                endTime = startTime.AddHours(1);
-                break;
-            case StatsInterval.Week:
-                var dayOfWeek = startTime.DayOfWeek;
-                var firstDayOfWeek = startTime.AddDays(-(int)dayOfWeek);
-                startTime = new DateTime(firstDayOfWeek.Year, firstDayOfWeek.Month, firstDayOfWeek.Day, 0, 0, 0);
-                endTime = startTime.AddDays(7);
-                break;
-            case StatsInterval.Month:
-                startTime = new DateTime(recordTime.Year, recordTime.Month, 1);
-                endTime = startTime.AddMonths(1);
-                break;
-            default:
-                startTime = new DateTime(recordTime.Year, recordTime.Month, recordTime.Day, 0, 0, 0);
-                endTime = startTime.AddDays(1);
-                break;
-        }
-
-        startTime = DateTime.SpecifyKind(startTime, DateTimeKind.Utc);
-        endTime = DateTime.SpecifyKind(endTime, DateTimeKind.Utc);
-        return (startTime, endTime);
-    }
-    #endregion
 }
