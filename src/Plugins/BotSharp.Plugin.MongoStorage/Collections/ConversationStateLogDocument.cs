16d3e92860b0de6c875e8dff243f162b87a54825
Jicheng Lu <103353@smsassist.com>
2025-01-29 16:21:19 -0600

    add entry agent id

--- src/Plugins/BotSharp.Plugin.MongoStorage/Collections/ConversationStateLogDocument.cs
+++ src/Plugins/BotSharp.Plugin.MongoStorage/Collections/ConversationStateLogDocument.cs
@@ -3,6 +3,7 @@ namespace BotSharp.Plugin.MongoStorage.Collections;
 public class ConversationStateLogDocument : MongoBase
 {
     public string ConversationId { get; set; }
+    public string AgentId { get; set; }
     public string MessageId { get; set; }
     public Dictionary<string, string> States { get; set; }
     public DateTime CreateTime { get; set; }
