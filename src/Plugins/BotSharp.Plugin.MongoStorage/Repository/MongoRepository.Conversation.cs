a1717d3b94c120c6038d202a340a42e6b115cba3
Jicheng Lu <103353@smsassist.com>
2025-02-12 10:19:32 -0600

    fix import

--- src/Plugins/BotSharp.Plugin.MongoStorage/Repository/MongoRepository.Conversation.cs
+++ src/Plugins/BotSharp.Plugin.MongoStorage/Repository/MongoRepository.Conversation.cs
@@ -1,5 +1,6 @@
 using BotSharp.Abstraction.Conversations.Models;
 using BotSharp.Abstraction.Repositories.Filters;
+using BotSharp.Core.Infrastructures;
 
 namespace BotSharp.Plugin.MongoStorage.Repository;
 
