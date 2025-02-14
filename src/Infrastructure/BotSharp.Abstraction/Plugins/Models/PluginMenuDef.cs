d824d16e69826ee74dadba34b760c652dc17ad99
chait <chait_software@hotmail.com>
2025-02-14 18:29:51 +0800

    [style] Code style adjustment.

--- src/Infrastructure/BotSharp.Abstraction/Plugins/Models/PluginMenuDef.cs
+++ src/Infrastructure/BotSharp.Abstraction/Plugins/Models/PluginMenuDef.cs
@@ -1,37 +1,29 @@
 namespace BotSharp.Abstraction.Plugins.Models;
 
-public class PluginMenuDef
+public class PluginMenuDef(string label, string? link = null, string? icon = null, int weight = 0)
 {
     [JsonIgnore]
-    public string Id { get; set; }
+    public string Id { get; set; } = default!;
 
-    public string Label { get; set; }
-
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public string? Icon { get; set; }
+    public string Label { get; set; } = label;
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public string? Link { get; set; }
+    public string? Icon { get; set; } = icon;
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Link { get; set; } = link;
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsHeader {  get; set; }
 
     [JsonIgnore]
-    public int Weight { get; set; }
+    public int Weight { get; set; } = weight;
 
     [JsonIgnore]
     public List<string>? Roles { get; set; }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<PluginMenuDef>? SubMenu {  get; set; }
-
-    public PluginMenuDef(string label, string? link = null, string? icon = null, int weight = 0) 
-    { 
-        Label = label;
-        Link = link;
-        Icon = icon;
-        Weight = weight;
-    }
 
     public override string ToString()
     {
