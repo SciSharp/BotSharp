80fc2f6c782fdecafc1f17de9fa3f016b8e9327f
Jicheng Lu <103353@smsassist.com>
2025-02-25 11:56:37 -0600

    add confidence

--- src/Plugins/BotSharp.Plugin.MongoStorage/Collections/ConversationDialogDocument.cs
+++ src/Plugins/BotSharp.Plugin.MongoStorage/Collections/ConversationDialogDocument.cs
@@ -4,6 +4,7 @@ public class ConversationDialogDocument : MongoBase
 {
     public string ConversationId { get; set; } = default!;
     public string AgentId { get; set; } = default!;
+    public string UserId { get; set; } = default!;
     public DateTime UpdatedTime { get; set; }
     public List<DialogMongoElement> Dialogs { get; set; } = [];
 }
