a537a647cdf0d4b15f94d3b7b27b4b9f6d1bcaaa
Haiping Chen <hchen@lessen.com>
2025-03-11 17:20:44 -0500

    politely ending phone call

--- src/Plugins/BotSharp.Plugin.Twilio/OutboundPhoneCallHandler/LlmContexts/LlmContextIn.cs
+++ src/Plugins/BotSharp.Plugin.Twilio/OutboundPhoneCallHandler/LlmContexts/LlmContextIn.cs
@@ -5,8 +5,8 @@ namespace BotSharp.Plugin.Twilio.OutboundPhoneCallHandler.LlmContexts;
 public class LlmContextIn
 {
     [JsonPropertyName("phone_number")]
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = null!;
 
     [JsonPropertyName("initial_message")]
-    public string InitialMessage { get; set; }
+    public string? InitialMessage { get; set; }
 }
