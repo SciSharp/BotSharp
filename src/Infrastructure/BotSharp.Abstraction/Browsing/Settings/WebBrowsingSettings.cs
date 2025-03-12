e570672a365d4fee9273fa3be1267067b0d4c76c
Visagan Guruparan <103048@smsassist.com>
2025-03-12 08:49:31 -0500

    Update default timeout to 30sec

--- src/Infrastructure/BotSharp.Abstraction/Browsing/Settings/WebBrowsingSettings.cs
+++ src/Infrastructure/BotSharp.Abstraction/Browsing/Settings/WebBrowsingSettings.cs
@@ -4,5 +4,6 @@ public class WebBrowsingSettings
 {
     public string Driver { get; set; } = "Playwright";
     public bool Headless { get; set; }
-    public float? DefaultTimeOut { get; set; }
+    // Default timeout in milliseconds
+    public float DefaultTimeOut { get; set; } = 30000;
 }
