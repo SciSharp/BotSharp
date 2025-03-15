292da2aee7244619be2d13de85d4e478c3cb6663
Haiping Chen <hchen@lessen.com>
2025-03-14 21:08:29 -0500

    Change template default to 0.8f

--- src/Infrastructure/BotSharp.Abstraction/Realtime/Models/RealtimeModelSettings.cs
+++ src/Infrastructure/BotSharp.Abstraction/Realtime/Models/RealtimeModelSettings.cs
@@ -2,7 +2,7 @@ namespace BotSharp.Abstraction.Realtime.Models;
 
 public class RealtimeModelSettings
 {
-    public float Temperature { get; set; } = 0.6f;
+    public float Temperature { get; set; } = 0.8f;
     public int MaxResponseOutputTokens { get; set; } = 512;
     public ModelTurnDetection TurnDetection { get; set; } = new();
 }
