439d4b451c19378bb3a2d01e46f0835ed29f59b4
Gunpal Jain <gunpal5@hotmail.com>
2025-03-23 23:57:58 +0530

    feat(Gemini): Added Text Embedding Model feat: implemented missing methods. changed to Google_GenerativeAI SDK which supports Multimodal Live APIs.

--- src/Plugins/BotSharp.Plugin.GoogleAI/Providers/ProviderHelper.cs
+++ src/Plugins/BotSharp.Plugin.GoogleAI/Providers/ProviderHelper.cs
@@ -1,15 +1,14 @@
 using LLMSharp.Google.Palm;
-using Mscc.GenerativeAI;
 
 namespace BotSharp.Plugin.GoogleAi.Providers;
 
 public static class ProviderHelper
 {
-    public static GoogleAI GetGeminiClient(string provider, string model, IServiceProvider services)
+    public static GenerativeAI.GoogleAi GetGeminiClient(string provider, string model, IServiceProvider services)
     {
         var settingsService = services.GetRequiredService<ILlmProviderService>();
         var settings = settingsService.GetSetting(provider, model);
-        var client = new GoogleAI(settings.ApiKey);
+        var client = new GenerativeAI.GoogleAi(settings.ApiKey);
         return client;
     }
 
