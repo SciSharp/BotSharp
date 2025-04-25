2b1587c02da733674af397d1bf0342203020c276
nick.yi <jqrddd@163.com>
2025-04-25 17:17:38 +0800

    optimize ICronTabHook

--- src/Infrastructure/BotSharp.Core.Crontab/Abstraction/ICrontabHook.cs
+++ src/Infrastructure/BotSharp.Core.Crontab/Abstraction/ICrontabHook.cs
@@ -1,20 +1,20 @@
 namespace BotSharp.Core.Crontab.Abstraction;
 
-public abstract class ICrontabHook
+public interface ICrontabHook
 {
-    public string[]? Triggers
+    string[]? Triggers
         => null;
 
-    public virtual void OnAuthenticate(CrontabItem item)
+    void OnAuthenticate(CrontabItem item)
     {
     }
 
-    public Task OnCronTriggered(CrontabItem item)
+    Task OnCronTriggered(CrontabItem item)
         => Task.CompletedTask;
 
-    public Task OnTaskExecuting(CrontabItem item)
+    Task OnTaskExecuting(CrontabItem item)
         => Task.CompletedTask;
 
-    public Task OnTaskExecuted(CrontabItem item)
+    Task OnTaskExecuted(CrontabItem item)
         => Task.CompletedTask;
 }
