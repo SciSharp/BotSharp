f82f36b9dd5fdbe250d977502581e62dbb63e5d3
Jicheng Lu <103353@smsassist.com>
2025-08-06 22:53:12 -0500

    minor change

--- src/Infrastructure/BotSharp.OpenAPI/ViewModels/Knowledges/Request/QueryVectorDataRequest.cs
+++ src/Infrastructure/BotSharp.OpenAPI/ViewModels/Knowledges/Request/QueryVectorDataRequest.cs
@@ -2,7 +2,7 @@ namespace BotSharp.OpenAPI.ViewModels.Knowledges.Request;
 
 public class QueryVectorDataRequest
 {
-    public IEnumerable<string> Ids { get; set; } = [];
+    public List<string> Ids { get; set; } = [];
     public bool WithVector { get; set; }
     public bool WithPayload { get; set; }
 }
