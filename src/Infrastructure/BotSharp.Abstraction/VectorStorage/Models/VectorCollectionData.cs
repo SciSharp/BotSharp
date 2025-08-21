584436498532217bf156614b9de3504d7621bc19
Jicheng Lu <103353@smsassist.com>
2025-08-21 15:44:10 -0500

    output payload

--- src/Infrastructure/BotSharp.Abstraction/VectorStorage/Models/VectorCollectionData.cs
+++ src/Infrastructure/BotSharp.Abstraction/VectorStorage/Models/VectorCollectionData.cs
@@ -4,6 +4,7 @@ public class VectorCollectionData
 {
     public string Id { get; set; }
     public Dictionary<string, VectorPayloadValue> Data { get; set; } = new();
+    public Dictionary<string, object> Payload => Data?.ToDictionary(x => x.Key, x => x.Value.DataValue) ?? [];
     public double? Score { get; set; }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
