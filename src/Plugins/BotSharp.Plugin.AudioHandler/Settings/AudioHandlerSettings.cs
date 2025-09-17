1b306b987c1bf8e9338bbf2294f94989f50edca4
Jicheng Lu <103353@smsassist.com>
2025-09-17 13:33:48 -0500

    refine audio handler

--- src/Plugins/BotSharp.Plugin.AudioHandler/Settings/AudioHandlerSettings.cs
+++ src/Plugins/BotSharp.Plugin.AudioHandler/Settings/AudioHandlerSettings.cs
@@ -1,5 +1,19 @@
+using BotSharp.Abstraction.Models;
+
 namespace BotSharp.Plugin.AudioHandler.Settings;
 
 public class AudioHandlerSettings
 {
+    public AudioSettings? Audio { get; set; }
 }
+
+#region Audio
+public class AudioSettings
+{
+    public AudioReadSettings? Reading { get; set; }
+}
+
+public class AudioReadSettings : LlmBase
+{
+}
+#endregion
