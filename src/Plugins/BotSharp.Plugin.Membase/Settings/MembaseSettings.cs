96ef895e25fddd2df7d0097a095e5125710fa959
nick.yi <jqrddd@163.com>
2026-03-18 17:33:46 +0800

    add membase timeout in setting

--- src/Plugins/BotSharp.Plugin.Membase/Settings/MembaseSettings.cs
+++ src/Plugins/BotSharp.Plugin.Membase/Settings/MembaseSettings.cs
@@ -5,4 +5,5 @@ public class MembaseSettings
     public string Host { get; set; } = "localhost";
     public string ProjectId { get; set; } = string.Empty;
     public string ApiKey { get; set; } = string.Empty;
+    public int TimeoutSecond { get; set; } = 10;
 }
