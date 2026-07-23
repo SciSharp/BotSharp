dc8098847097e1b7e2e8f6f04f54823e34ec06c2
nick.yi <jqrddd@163.com>
2026-07-23 11:02:45 +0800

    clean code

--- src/Infrastructure/BotSharp.Core/Routing/Functions/RouteToAgentFn.cs
+++ src/Infrastructure/BotSharp.Core/Routing/Functions/RouteToAgentFn.cs
@@ -34,13 +34,6 @@ public partial class RouteToAgentFn : IFunctionCallback
             {
                 states.SetState("user_goal_agent", args.OriginalAgent, isNeedVersion: true);
             }
-            //Using AI results, no correction is needed.
-            //else if (args.OriginalAgent == args.AgentName && args.OriginalAgent != goalAgentInState)
-            //{
-            //    // Correct to original agent
-            //    args.OriginalAgent = goalAgentInState;
-            //    correctToOriginalAgent = true;
-            //}
             else if (args.OriginalAgent != args.AgentName && args.OriginalAgent != goalAgentInState)
             {
                 // Correct to original agent
