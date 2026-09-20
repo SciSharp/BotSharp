c9a9ed43d8e985dfaa21834038026b41b5114eca
nick.yi <jqrddd@163.com>
2026-09-20 15:15:48 +0800

    optimize RouteToAgentFn Indication

--- src/Infrastructure/BotSharp.Core/Routing/Functions/RouteToAgentFn.cs
+++ src/Infrastructure/BotSharp.Core/Routing/Functions/RouteToAgentFn.cs
@@ -12,7 +12,12 @@ public partial class RouteToAgentFn : IFunctionCallback
     public string Name => "route_to_agent";
     private readonly IServiceProvider _services;
     private readonly IRoutingContext _context;
+#if DEBUG
     public string Indication => "Routing to the specified AI agent...";
+#else
+    public string Indication => "Think...";
+#endif
+
     public RouteToAgentFn(IServiceProvider services, IRoutingContext context)
     {
         _services = services;
